603a0453c8a8259771a184c9c9147d80c00fd076
Jayaprakash J <prakashstr.vdm@gmail.com>
2024-11-29 16:15:28 +0530

    04/12/2024

---
+++ src/CyclicSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr={4,3,2,7,8,2,3,1};
+        cycleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void cycleSort(int[] arr){
+        int i =0;
+        while(i< arr.length){
+            int correctIndex=arr[i]-1;
+            if(arr[i]!=arr[correctIndex]){
+                swap(arr, i , correctIndex);
+            }else{
+                i++;
+            }
+        }
+    }
+    public static void swap(int[] arr, int index, int correctIndex){
+        int temp =arr[correctIndex];
+        arr[correctIndex]=arr[index];
+        arr[index]=temp;
+
+    }
+}
