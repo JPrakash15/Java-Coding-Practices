eda0d3bf14bb6fb5fac08a585091a7ff568c226a
Jayaprakash J <prakashstr.vdm@gmail.com>
2024-12-11 17:51:01 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/CyclicSort.java

--- src/BitWise.java
+++ src/BitWise.java
@@ -5,6 +5,20 @@ public class BitWise {
 
         int[] nums={3,3,3,3};
         System.out.println(findUnique(nums));
+
+
+        int n=10;
+
+        int ans=0;
+        int base=5;
+        while(n>0){
+            int last=n&1;
+            n=n>>1;
+            ans+=last*base;
+            base=base*5;
+        }
+
+        System.out.println(ans);
     }
 
     static boolean findOddEven(int n){
